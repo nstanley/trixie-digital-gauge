5177ac3ac46aad4b6bb1c9ce169ead78803f5f29
Nick Stanley <nstanleyiv@gmail.com>
2020-07-27 09:05:17 -0500

    some screen demo

--- code/trixie.py
+++ code/trixie.py
@@ -1,5 +1,7 @@
 # Main program to run the OBD Gauge
 
+import time
+import subprocess
 import digitalio
 import board
 from trixie_view import TrixieView
@@ -16,7 +18,20 @@ class TrixieController():
     def __init__(self):
         self.view = TrixieView(cs_pin, dc_pin, reset_pin, BAUDRATE)
         self.view.showSplash("/home/pi/trixie-digital-gauge/resources/audi_128x96.png")
-        #self.view.showData("Speed", 37)
+        time.sleep(3.2)
+        self.view.showData("Speed", 37)
+        time.sleep(3.2)
+        self.view.showSplash("/home/pi/trixie-digital-gauge/resources/audi_128x96-big.png")
+        time.sleep(3.2)
+        self.view.showData("RPM", 2237)
+        time.sleep(3.2)
+        self.view.showSplash("/home/pi/trixie-digital-gauge/resources/audi_128x96-red.png")
+        time.sleep(3.2)
+        self.view.showData("Load", 14)
+        time.sleep(3.2)
+        self.view.showSplash("/home/pi/trixie-digital-gauge/resources/audi_128x96-big-red.png")
+        time.sleep(3.2)
+        self.view.showData("Engine Temp", 190)
 
 def main():
     print("Trixie Digital Gauge Startup!")
